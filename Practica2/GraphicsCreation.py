160dca7109fecd16794e2eddb3a47245fed585a4
Pablo Seijo <105173542+Vehkzyr@users.noreply.github.com>
2023-05-01 13:24:41 +0200

    Codigos para las graficas actualizados

--- Practica2/GraphicsCreation.py
+++ Practica2/GraphicsCreation.py
@@ -11,59 +11,36 @@ import os
 N_parameters = [250, 500, 750, 1000, 1500, 2000, 2550, 3000]
 
 # Lee los valores desde el archivo de texto
-with open('grafica.txt', 'r') as f:
+with open('resultados_apartado1-O3.txt', 'r') as f:
     values = [float(x) for x in f.readlines()]
 
 # crear un array de arrays dividido en los grupos de 10 necesarios para la realizacion de
 # las graficas, ergo groups[0] = {list: 10} [...], y asi hasta los 8 grupos necesarios, para los diferentes valores de N
 groups = [values[i:i + 10] for i in range(0, len(values), 10)]
 
-# Crea una gráfica con múltiples líneas para las primeras 4 N
-fig1, ax1 = plt.subplots()
+# Calcula la mediana de cada grupo de valores
+medians = [sorted(group)[5] for group in groups]
 
-#Ajustamos el tamaño de la figura
-fig1.set_size_inches(12, 8)
+# Crea una gráfica con los valores de la mediana en el eje y
+fig, ax = plt.subplots()
 
-# Añade cada conjunto de datos como una nueva línea en la misma gráfica
-for i, group in enumerate(groups[:4]):
-    ax1.plot(range(1, 11), group, marker = 'o', markersize = 5, label = f'N = {N_parameters[i]}')
+# Ajustamos el tamaño de la figura
+fig.set_size_inches(12, 8)
 
-# Título de la gráfica
-ax1.set_title('Tics de reloj para las primeras 4 N')
+# Establecemos el estilo de línea a "None"
+ax.plot(N_parameters, medians, color='crimson', marker='o', markersize = 5)
 
-# Nombre de los ejes x e y
-ax1.set_xlabel('Número de ejecución')
-ax1.set_ylabel('Tics de reloj')
-
-# Añadimos la leyenda
-ax1.legend(loc = 'best')
-
-# Guardamos la gráfica en un archivo png
-plt.savefig('grafica1.png')
-
-# Crea una gráfica con múltiples líneas para las últimas 4 N
-fig2, ax2 = plt.subplots()
-
-#Ajustamos el tamaño de la figura
-fig2.set_size_inches(12, 8)
-
-colors = ['b', 'g', 'r', 'm']  # colores diferentes para cada línea
-
-# Añade cada conjunto de datos como una nueva línea en la misma gráfica
-for i, group in enumerate(groups[4:]):
-    ax2.plot(range(1, 11), group, color=colors[i], marker='o', markersize=5, label=f'N = {N_parameters[i+4]}')
+# Configuramos los ticks del eje y en notación científica
+ax.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
 
 # Título de la gráfica
-ax2.set_title('Tics de reloj para las últimas 4 N')
+ax.set_title('Mediana de los tics de reloj para todas las N en la tercera ejecucion del apartado 1')
 
 # Nombre de los ejes x e y
-ax2.set_xlabel('Número de ejecución')
-ax2.set_ylabel('Tics de reloj')
-
-# Añadimos la leyenda
-ax2.legend(loc = 'best')
+ax.set_xlabel('Valor de N')
+ax.set_ylabel('Tics de reloj (En notacion cientifica)')
 
 # Guardamos la gráfica en un archivo png
-plt.savefig('grafica2.png')
+plt.savefig('GraficaApartado1-O3-mediana.png')
 
-print("Por favor, compruebe que se han generado los archivos grafica1.png y grafica2.png en la siguiente lista: ", os.listdir())
+print("Por favor, compruebe que se ha generado el archivo GraficaApartado1-O0-mediana.png en la siguiente lista: ", os.listdir())
--- Practica2/graficasColapsadas.py
+++ Practica2/graficasColapsadas.py
@@ -15,70 +15,66 @@ import os
 N_parameters = [250, 500, 750, 1000, 1500, 2000, 2550, 3000]
 
 # Lee los valores desde el archivo de texto de los valores del apartado 1
-with open('datosAp1.txt', 'r') as f:
+with open('resultados_apartado1-O0.txt', 'r') as f:
     valuesAp1 = [float(x) for x in f.readlines()]
 
 # Lee los valores desde el archivo de texto de los valores del apartado 2
-with open('datosAp2.txt', 'r') as fp:
+with open('resultados_apartado1-O2.txt', 'r') as fp:
     valuesAp2 = [float(x) for x in fp.readlines()]
 
 # Lee los valores desde el archivo de texto de los valores del apartado 3
-with open('datosAp3.txt', 'r') as fpd:
+with open('resultados_apartado1-O3.txt', 'r') as fpd:
     valuesAp3 = [float(x) for x in fpd.readlines()]
-
-# Lee los valores desde el archivo de texto de los valores del apartado 4
-with open('datosAp4.txt', 'r') as fpd4:
-    valuesAp4 = [float(x) for x in fpd4.readlines()]
 
 # crear un array de arrays dividido en los grupos de 10 necesarios para la realizacion de
 # las graficas, ergo groups[0] = {list: 10} [...], y asi hasta los 8 grupos necesarios, para los diferentes valores de N
 groupsAp1 = [valuesAp1[i:i + 10] for i in range(0, len(valuesAp1), 10)]
 groupsAp2 = [valuesAp2[i:i + 10] for i in range(0, len(valuesAp2), 10)]
 groupsAp3 = [valuesAp3[i:i + 10] for i in range(0, len(valuesAp3), 10)]
-groupsAp4 = [valuesAp4[i:i + 10] for i in range(0, len(valuesAp4), 10)]
+
+# Calcula la mediana de cada grupo de valores
+medians1 = [sorted(group)[5] for group in groupsAp1]
+medians2 = [sorted(group)[5] for group in groupsAp2]
+medians3 = [sorted(group)[5] for group in groupsAp3]
 
 # Crea una gráfica con múltiples líneas
 fig, ax = plt.subplots()
 
-#Ajustamos el tamaño de la figura
-fig.set_size_inches(7, 5)
+# Ajustamos el tamaño de la figura
+fig.set_size_inches(12, 8)
 
 # Añade cada conjunto de datos como una nueva línea en la misma gráfica
 for i, group in enumerate(groupsAp1):
     if i == 0:
-        ax.plot(range(1, 11), group, marker='o', markersize=5, color='gold',label = 'Apartado 1')
+        ax.plot(N_parameters, medians1, marker='o', markersize=5, color='gold',label = 'Primera Ejecucion')
     else:
-        ax.plot(range(1, 11), group, marker='o', markersize=5, color='gold')
+        ax.plot(N_parameters, medians1, marker='o', markersize=5, color='gold')
 
 
 # Añade cada conjunto de datos como una nueva línea en la misma gráfica
 for i, group in enumerate(groupsAp2):
     if i == 0:
-        ax.plot(range(1, 11), group, marker='o', markersize=5, color='blueviolet', label='Apartado 2')
+        ax.plot(N_parameters, medians2, marker='o', markersize=5, color='blueviolet', label='Segunda Ejecucion')
     else:
-        ax.plot(range(1, 11), group, marker='o', markersize=5, color='blueviolet')
+        ax.plot(N_parameters, medians2, marker='o', markersize=5, color='blueviolet')
 
 
 # Añade cada conjunto de datos como una nueva línea en la misma gráfica
 for i, group in enumerate(groupsAp3):
     if i == 0:
-        ax.plot(range(1, 11), group, marker='o', markersize=5, color='mediumturquoise', label='Apartado 3')
+        ax.plot(N_parameters, medians3, marker='o', markersize=5, color='crimson', label='Tercera Ejecucion')
     else:
-        ax.plot(range(1, 11), group, marker='o', markersize=5, color='mediumturquoise')
+        ax.plot(N_parameters, medians3, marker='o', markersize=5, color='crimson')
 
-# Añade cada conjunto de datos como una nueva línea en la misma gráfica
-for i, group in enumerate(groupsAp4):
-    if i == 0:
-        ax.plot(range(1, 11), group, marker='o', markersize=5, color='crimson', label='Apartado 4')
-    else:
-        ax.plot(range(1, 11), group, marker='o', markersize=5, color='crimson')
+# Configuramos los ticks del eje y en notación científica
+ax.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
 
 # Título de la gráfica
-ax.set_title('Número de ciclos por cada tamalo de matriz')
+ax.set_title('Mediana de los tics de reloj para todas las N en el apartado 1')
 
 # Nombre de los ejes x e y
-ax.set_xlabel('Parámetros de N')
-ax.set_ylabel('Número de ciclos')
+ax.set_xlabel('Valor de N')
+ax.set_ylabel('Tics de reloj (En notacion cientifica)')
 
 # Añadimos la leyenda
 ax.legend(loc = 'best')
